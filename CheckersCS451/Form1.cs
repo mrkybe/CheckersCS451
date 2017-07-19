6806c1d3715eccae17bd2939a343d5c61b4b85cf
Camden Marchetti <camden.marchetti@gmail.com>
2017-07-18 20:48:47 -0400

    Work in CheckersCS451 project to create WinForm for game board and implement images for pieces and board

--- CheckersCS451/Form1.cs
+++ CheckersCS451/Form1.cs
@@ -1,12 +1,42 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace CheckersCS451
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-    }
+
+	public partial class Form1 : Form
+	{
+
+		private Bitmap image_pieceRed, image_pieceBlack;
+
+		public Form1()
+		{
+			InitializeComponent();
+			initHook();
+		}
+
+		private void initHook()
+		{
+			try
+			{
+				Assembly exAss = Assembly.GetExecutingAssembly();
+
+				Stream bgImg = exAss.GetManifestResourceStream("CheckersCS451.Resources.board.png");
+				Stream pcRed = exAss.GetManifestResourceStream("CheckersCS451.Resources.piece_red.png");
+				Stream pcBlk = exAss.GetManifestResourceStream("CheckersCS451.Resources.piece_black.png");
+
+
+				// TODO: Fix background image (8x8 does not stretch as expected)
+				this.board.BackgroundImage = new Bitmap(bgImg);
+				this.image_pieceRed = new Bitmap(pcRed);
+				this.image_pieceBlack = new Bitmap(pcBlk);
+			}
+			catch
+			{
+				/* TODO: Handle issues with loading board or piece images? */
+			}
+		}
+	}
 }
