a640ba30cea4d76c2b1c262996c790a4440b6f8b
rkay94 <rkay94@gmail.com>
2017-08-04 19:48:29 -0400

    -server side sockets setup

--- CheckersServer/Program.cs
+++ CheckersServer/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
 using Data;
 using System.Drawing;
+using System.Linq;
+using System.Net;
 
 namespace CheckersServer
 {
     class Program
     {
+        private static List<ConnectedClient> clients;
+        private static List<CheckersGM> games;
         static void Main(string[] args)
         {
-            //var games = new List<CheckersGM> {new CheckersGM()};
+            clients = new List<ConnectedClient>();
+            games = new List<CheckersGM>();
+
+            SetupSockets();
+        }
+
+        public static void SetupSockets()
+        {
+            int clientCount = 0;
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 1337);
+
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    CheckersGM gameBoard;
+                    if (clientCount % 2 == 0)
+                    {
+                        gameBoard = new CheckersGM();
+                        games.Add(gameBoard);
+                    }
+                    else
+                    {
+                        gameBoard = games.Last();
+                    }
+                    clientCount++;
+                    ConnectedClient client = new ConnectedClient(handler, gameBoard,  clientCount.ToString());
+                    clients.Add(client);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nServer ending, press enter to close terminal.");
+            Console.Read();
+        }
+
+        static void LocalTestMain(string[] args)
+        {
             CheckersGM gm = new CheckersGM();
             while (true)
             {
